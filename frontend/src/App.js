134dca35aef7aab492b3d0cae0b1eeb3f29168ed
Jeeraphat <jeeraphat1742@gmail.com>
2022-09-26 00:20:17 +0700

    step 5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from './data'
+
 function App() {
 	return (
 		<div>
 			<header>
 				<a href="/">POSME</a>
 			</header>
-			<main>List Products</main>
+			<main>
+				<h1>Featured Products</h1>
+				<div className="products">
+					{data.products.map((products) => (
+						<div className="product" key={products.slug}>
+							<a href={`/product/${products.slug}`}>
+								<img src={products.image} alt={products.name} />
+							</a>
+							<div className="product-info">
+								<a href={`/product/${products.slug}`}>
+									<p>{products.name}</p>
+								</a>
+								<p>
+									<strong>${products.price}</strong>
+								</p>
+								<button>Add to cart</button>
+							</div>
+						</div>
+					))}
+				</div>
+			</main>
 		</div>
 	)
 }
